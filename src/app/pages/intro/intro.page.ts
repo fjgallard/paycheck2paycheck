2f767a6f119efb2debcd8b4a5f6157b754efcede
fjgallard <fjdgallardo@gmail.com>
2020-08-31 16:29:03 +0800

    Set up income slide.

--- src/app/pages/intro/intro.page.ts
+++ src/app/pages/intro/intro.page.ts
@@ -10,11 +10,17 @@ import { Router } from '@angular/router';
 })
 export class IntroPage implements OnInit {
 
+  private readonly DEFAULT_INCOME = 1000;
+
   @ViewChild(IonSlides) slides: IonSlides;
+
+  income: number;
 
   constructor(private storage: Storage, private router: Router) { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    console.log(await this.storage.get('income'));
+    console.log(await this.storage.get('tutorialSeen'));
   }
 
   next() {
@@ -22,8 +28,15 @@ export class IntroPage implements OnInit {
   }
 
   async finish() {
-    await this.storage.set('tutorialSeen', true);
-    this.router.navigateByUrl('/login');
+    if (!this.income) {
+      this.income = this.DEFAULT_INCOME;
+    }
+
+    await Promise.all([
+      this.storage.set('income', this.income),
+      this.storage.set('tutorialSeen', true)
+    ]);
+    this.router.navigateByUrl('/intro');
   }
 
 }
