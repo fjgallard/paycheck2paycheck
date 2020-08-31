3c2b1d631456f88124620a9cd226dea4828d71cf
fjgallard <fjdgallardo@gmail.com>
2020-08-31 16:53:09 +0800

    Created intro guard.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,16 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { IntroGuard } from '@guards/intro.guard';
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'intro',
-    pathMatch: 'full'
-  },
   {
     path: 'intro',
     loadChildren: () => import('./pages/intro/intro.module').then( m => m.IntroPageModule)
   },
   {
     path: '',
-    loadChildren: () => import('./containers/main-menu/main-menu.module').then( m => m.MainMenuPageModule)
+    loadChildren: () => import('./containers/main-menu/main-menu.module').then( m => m.MainMenuPageModule),
+    canActivate: [IntroGuard]
   }
 ];
 
---
+++ src/app/guards/intro.guard.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Storage } from '@ionic/storage';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IntroGuard implements CanActivate {
+
+  constructor(private storage: Storage, private router: Router) { }
+
+  async canActivate(): Promise<boolean> {
+      const tutorialSeen = await this.storage.get('tutorialSeen');
+
+      if (!tutorialSeen) {
+        this.router.navigateByUrl('/intro');
+      }
+
+      return tutorialSeen;
+  }
+
+}
