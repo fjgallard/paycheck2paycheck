a133a1a173127ae7ec284380ed2a16cf8b9a48c9
fjgallard <fjdgallardo@gmail.com>
2020-09-02 19:59:56 +0800

    Set a custom monthly income.

--- src/app/pages/settings/settings.page.ts
+++ src/app/pages/settings/settings.page.ts
@@ -9,14 +9,27 @@ import { Storage } from '@ionic/storage';
 export class SettingsPage implements OnInit {
 
   monthlyIncome: number;
+  customIncome: number;
 
   constructor(private storage: Storage) { }
 
   async ngOnInit() {
+    const date = new Date();
+    const incomeForTheMonth = (date.getMonth() + 1) + '-' + date.getDate() + '-' + date.getFullYear();
     this.monthlyIncome = await this.storage.get('monthlyIncome');
+    this.customIncome = await this.storage.get(`${incomeForTheMonth}-income`);
   }
 
-  submit() {
+  updateCustomIncome() {
+    if (this.customIncome) {
+      const date = new Date();
+      const incomeForTheMonth = (date.getMonth() + 1) + '-' + date.getDate() + '-' + date.getFullYear();
+
+      this.storage.set(`${incomeForTheMonth}-income`, this.customIncome);
+    }
+  }
+
+  updateMonthlyIncome() {
     if (this.monthlyIncome) {
       this.storage.set('monthlyIncome', this.monthlyIncome);
       // set current income to this income
