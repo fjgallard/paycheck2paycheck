1f1d5dddb3eaf2beb8a439572ebad64f5a857461
fjgallard <fjdgallardo@gmail.com>
2020-09-19 15:02:27 +0800

    Setup expense form control.

--- src/app/pages/expense/expense.page.ts
+++ src/app/pages/expense/expense.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { Expense } from '@services/storage/expense.service';
 
 @Component({
   selector: 'app-expense',
@@ -10,9 +13,19 @@ export class ExpensePage implements OnInit {
   expenseIcon: string;
 
   id         : string;
+  expense    : Expense;
+  expenseForm: FormGroup
 
-  constructor() {
+  constructor(
+    private fb: FormBuilder,
+    private route: ActivatedRoute
+  ) {
     this.expenseIcon = 'wallet';
+    this.expenseForm = this.fb.group({
+      value    : [ , Validators.required ],
+      createdAt: [ '', Validators.required ],
+      category : [ '' ]
+    });
   }
 
   ngOnInit() {
