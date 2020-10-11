1d96c6b6ca945f2303e7a21cc5778bb15350d10d
fjgallard <fjdgallardo@gmail.com>
2020-10-11 13:54:20 +0800

    Added Icons

--- src/app/pages/budgets/budget/budget.component.ts
+++ src/app/pages/budgets/budget/budget.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { Budget } from '@services/storage/budget.service';
 
 @Component({
   selector: 'app-budget',
@@ -7,8 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BudgetComponent implements OnInit {
 
-  constructor() { }
+  id: string;
+  budget: Budget;
+
+  iconList = {
+    airplane: false,
+    home: false,
+    pizza: false,
+    card: false,
+    bus: false,
+    basket: false,
+    bulb: false,
+    call: false
+  };
+
+  iconListKeys = [];
+
+  constructor(private modalCtrl: ModalController) {
+    this.iconListKeys = Object.keys(this.iconList);
+  }
 
   ngOnInit() {}
+
+  closeModal() {
+    this.modalCtrl.dismiss({
+      id: this.id,
+      budget: this.budget
+    });
+  }
+
+  setIcon(icon: string) {
+    // this.categoryForm.get('icon').setValue(icon);
+    // this.deselectIcons();
+    this.iconList[icon] = true;
+  }
 
 }
