71f67bbbe52db1669282c6a937fa3600c0b632f3
fjgallard <fjdgallardo@gmail.com>
2020-10-14 21:38:19 +0800

    Used dashbord btns for routes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,9 +14,40 @@ const routes: Routes = [
     canActivate: [IntroGuard]
   },
   {
-    path: '',
-    loadChildren: () => import('./containers/main-tab/main-tab.module').then( m => m.MainTabPageModule),
-    canActivate: [IntroGuard]
+    path: 'dashboard',
+    loadChildren: () => import('@pages/dashboard/dashboard.module').then( m => m.DashboardPageModule)
+  },
+  {
+    path: 'settings',
+    loadChildren: () => import('@pages/settings/settings.module').then( m => m.SettingsPageModule)
+  },
+  {
+    path: 'budgets',
+    loadChildren: () => import('@pages/budgets/budgets.module').then( m => m.BudgetsPageModule)
+  },
+  {
+    path: 'category/new',
+    loadChildren: () => import('@pages/category/category.module').then( m => m.CategoryPageModule)
+  },
+  {
+    path: 'category/:id',
+    loadChildren: () => import('@pages/category/category.module').then( m => m.CategoryPageModule)
+  },
+  {
+    path: 'categories',
+    loadChildren: () => import('@pages/categories/categories.module').then( m => m.CategoriesPageModule)
+  },
+  {
+    path: 'expenses',
+    loadChildren: () => import('@pages/expenses/expenses.module').then( m => m.ExpensesPageModule)
+  },
+  {
+    path: 'expense/new',
+    loadChildren: () => import('@pages/expense/expense.module').then( m => m.ExpensePageModule)
+  },
+  {
+    path: 'expense/:id',
+    loadChildren: () => import('@pages/expense/expense.module').then( m => m.ExpensePageModule)
   }
 ];
 
